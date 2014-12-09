604771e0a08ccb21c32b3973e9cff3951dbb1fb4
Christian Theune <ct@gocept.com>
2014-12-09 13:22:43 +0100

    Initial boiler-plate checkin.

---
+++ setup.py
@@ -0,0 +1,46 @@
+"""A library of components for batou.
+"""
+
+from setuptools import setup, find_packages
+import glob
+import os.path
+
+
+def project_path(*names):
+    return os.path.join(os.path.dirname(__file__), *names)
+
+setup(
+    name='batou_ext',
+    version='0.1dev0',
+    install_requires=[
+        'batou>=1.0b20',
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+        ],
+    },
+    author='Christian Theune <ct@gocept.com>',
+    author_email='ct@gocept.com',
+    license='BSD (2-clause)',
+    url='https://plan.flyingcircus.io/projects/batou/',
+    keywords='deployment',
+    classifiers="""\
+License :: OSI Approved :: BSD License
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 2 :: Only
+"""[:-1].split('\n'),
+    description=__doc__.strip(),
+    long_description='\n\n'.join(open(project_path(name)).read() for name in (
+        'README',
+        'CHANGES.txt',
+        'HACKING.txt',
+        )),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    data_files=[('', glob.glob(project_path('*.txt')))],
+    zip_safe=False,
+)
