ab5bde413dff9411ad1ea4993e74adbdcc290cc8
Christian Zagrodnick <cz@flyingcircus.io>
2016-05-31 13:54:56 +0200

    add fc-nix rebuild helper

--- src/batou_ext/nix.py
+++ src/batou_ext/nix.py
@@ -20,3 +20,29 @@ class Package(batou.component.Component):
 
     def update(self):
         self.cmd('nix-env -i {}'.format(self.package))
+
+
+class Rebuild(batou.component.Component):
+    """Trigger rebuild on FC platform.
+
+    Usage::
+
+        # Tirgger rebuild if self or subcomponent changed
+        self += batou_ext.nix.Rebuild()
+
+        # Trigger rebuild if specific components changed:
+        self += batou_ext.nix.Rebuild(dependencies=(self, foo, bar))
+
+    """
+
+    dependencies = None
+
+    def verify(self):
+        if self.dependencies:
+            for dependency in self.dependencies:
+                dependency.assert_no_changes()
+        else:
+            self.parent.assert_no_subcomponent_changes()
+
+    def update(self):
+        self.cmd('sudo systemctl start fc-manage')
