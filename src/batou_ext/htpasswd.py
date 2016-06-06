8742900dd91057908d844c1e90aaa8056a67f541
Frank Lanitz <fl@flyingcircus.io>
2016-06-06 11:00:08 +0200

    Adding a component for htpasswd

---
+++ src/batou_ext/htpasswd.py
@@ -0,0 +1,19 @@
+import batou
+import batou.component
+import batou.lib.file
+import passlib
+
+
+class HTPasswd(batou.component.Component):
+
+    namevar = 'path'
+    users = ''
+
+    def configure(self):
+        users = []
+        for line in self.users.split('\n'):
+            line = line.strip()
+            user, password = line.split(':')
+            password = passlib.hash.sha512_crypt.encrypt(password)
+            users.append('{}:{}'.format(user, password))
+        self += batou.lib.file.File(self.path, content='\n'.join(users))
