6fb4fc4bcbc6bf6d1420f629db17bf807dcefe72
Frank Lanitz <fl@flyingcircus.io>
2016-06-14 13:02:59 +0200

    Adding a component for setting ACL

---
+++ src/batou_ext/acl.py
@@ -0,0 +1,23 @@
+import batou
+import batou.component
+import batou.lib.file
+
+
+class ACL(batou.component.Component):
+    path = ''
+    # A list of rules for setfacl. Each rule e.g. user::rwx needs to be
+    # one element of the list.
+    ruleset = []
+
+    def update(self):
+        proc = self.cmd('setfacl --set-file=- "{}"'.format(self.path),
+                communicate=False)
+        outs, errs = proc.communicate(input='\n'.join(self.ruleset))
+
+    def verify(self):
+        proc = self.cmd(
+            'getfacl -cpE {}'.format(self.path),
+            communicate=False)
+        outs, errs = proc.communicate()
+        if outs.strip() != '\n'.join(self.ruleset):
+            raise batou.UpdateNeeded
