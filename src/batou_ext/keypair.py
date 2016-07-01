f2547e66cebe1ff667101234eac76a0997f21aa6
Frank Lanitz <fl@flyingcircus.io>
2016-07-01 15:09:00 +0200

    KeyPair: Add a base_path for certificates.
    Default is empty so it should keep compatible with current behavior. But this allows to put certs explicit to e.g. /etc/nginx/local or /etc/local/nginx/

--- src/batou_ext/keypair.py
+++ src/batou_ext/keypair.py
@@ -1,6 +1,8 @@
 from batou.component import Component, Attribute
 from batou.lib.file import File
 
+import os
+
 
 class KeyPair(Component):
 
@@ -9,14 +11,18 @@ class KeyPair(Component):
     crt = None
     key = None
 
+    base_path = Attribute(str, '')
     provide_itself = Attribute(bool, True)
 
     def configure(self):
-        self.crt_file = File('{}.crt'.format(self.name), content=self.crt)
+        self.crt_file = File(
+            os.path.join(self.base_path, '{}.crt'.format(self.name)),
+            content=self.crt)
         self += self.crt_file
-        self.key_file = File('{}.key'.format(self.name),
-                             content=self.key,
-                             mode=0o600)
+        self.key_file = File(
+            os.path.join(self.base_path, '{}.key'.format(self.name)),
+            content=self.key,
+            mode=0o600)
         self += self.key_file
 
         if self.provide_itself:
