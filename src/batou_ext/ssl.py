9be332034bab30e4bc399946b8fa207e90d9b898
Frank Lanitz <fl@flyingcircus.io>
2017-05-11 10:18:00 +0200

    Extend ssl.Certificate component to allow both given SSL certificate as well as usage of letsencrypt with a common interface

--- src/batou_ext/ssl.py
+++ src/batou_ext/ssl.py
@@ -3,9 +3,12 @@ import batou.lib.cron
 import batou.lib.download
 import batou.lib.file
 import pkg_resources
+import os.path
+
 
 class Certificate(batou.component.Component):
 
+    # Let's Encrypt
     dehydrated_url = (
         "https://raw.githubusercontent.com/lukas2511/dehydrated/"
         "b36d638a910ce7c6be0bb8330d1d945a653f70af/dehydrated")
@@ -20,42 +23,64 @@ class Certificate(batou.component.Component):
     # TODO: make depend on domainname
     refresh_timing = '24 9 * * *'
 
+    # Optinal if you are having a valid certificate and don't want to
+    # make usage of letsencrypt
+    key_content = None
+    crt_content = None
+    use_letsencrypt = batou.component.Attribute('literal', True)
+
     def configure(self):
-        self += batou.lib.download.Download(
-            self.dehydrated_url,
-            checksum=self.dehydrated_checksum,
-            target='dehydrated')
-        self += batou.lib.file.Mode('dehydrated', mode=0o755)
+        if self.key_content and not self.use_letsencrypt:
+            self.crt_file = batou.lib.file.File(
+                os.path.join('{}/{}.crt'.format(self.workdir, self.domain)),
+                content=self.crt_content)
+            self += self.crt_file
+            self.key_file = batou.lib.file.File(
+                os.path.join('{}/{}.key'.format(self.workdir, self.domain)),
+                content=self.key_content,
+                mode=0o600)
+            self += self.key_file
 
-        if not self.wellknown and self.docroot:
-            self.wellknown = '{}/.well-known/acme-challenge'.format(
-                self.docroot)
-        self += batou.lib.file.File(
-            self.wellknown, ensure='directory', leading=True)
+            self.key = self.key_file.path
+            self.fullchain = self.crt_file.path
 
-        self += batou.lib.file.File(
-            'cert-{}.conf'.format(self.domain),
-            content='WELLKNOWN={}'.format(self.wellknown))
-        self.config = self._
+        else:
+            self += batou.lib.download.Download(
+                self.dehydrated_url,
+                checksum=self.dehydrated_checksum,
+                target='dehydrated')
+            self += batou.lib.file.Mode('dehydrated', mode=0o755)
 
-        self += batou.lib.file.File(
-            'cert-{}.sh'.format(self.domain),
-            content=pkg_resources.resource_string(
-                'batou_ext', 'resources/cert.sh'),
-            mode=0o700)
-        self.cert_sh = self._
+            if not self.wellknown and self.docroot:
+                self.wellknown = '{}/.well-known/acme-challenge'.format(
+                    self.docroot)
+            self += batou.lib.file.File(
+                self.wellknown, ensure='directory', leading=True)
 
-        self += batou.lib.cron.CronJob(
-            self.cert_sh.path,
-            timing=self.refresh_timing,
-            logger='cert-update')
+            self += batou.lib.file.File(
+                'cert-{}.conf'.format(self.domain),
+                content='WELLKNOWN={}'.format(self.wellknown))
+            self.config = self._
 
-        self.key = "{}/{}/privkey.pem".format(self.workdir, self.domain)
-        self.fullchain = "{}/{}/fullchain.pem".format(self.workdir,
-                                                      self.domain)
+            self += batou.lib.file.File(
+                'cert-{}.sh'.format(self.domain),
+                content=pkg_resources.resource_string(
+                    'batou_ext', 'resources/cert.sh'),
+                mode=0o700)
+            self.cert_sh = self._
+
+            self += batou.lib.cron.CronJob(
+                self.cert_sh.path,
+                timing=self.refresh_timing,
+                logger='cert-update')
+
+            self.key = "{}/{}/privkey.pem".format(self.workdir, self.domain)
+            self.fullchain = "{}/{}/fullchain.pem".format(self.workdir,
+                                                          self.domain)
 
     def verify(self):
         self.assert_no_subcomponent_changes()
 
     def update(self):
-        self.cmd(self.cert_sh.path)
+        if self.use_letsencrypt:
+            self.cmd(self.cert_sh.path)
