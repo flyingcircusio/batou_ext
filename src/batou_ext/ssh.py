0517d5bcaaf638663c7de2164c75699292ca33b1
Frank Lanitz <fl@flyingcircus.io>
2017-05-11 18:51:32 +0200

    Make ssh.SSHKeyPair component also support ed25519 keys

--- src/batou_ext/ssh.py
+++ src/batou_ext/ssh.py
@@ -1,6 +1,6 @@
 from batou import UpdateNeeded
 from batou.component import Component, Attribute
-from batou.lib.file import Directory, File
+from batou.lib.file import Directory, File, Purge
 import batou.c
 import os
 import os.path
@@ -11,12 +11,19 @@ class SSHKeyPair(Component):
     """Install SSH user and host keys.
 
     User keys are read from the secrets file and written to
-    ~/.ssh/id_rsa{,.pub}.  """
+    ~/.ssh/id_rsa{,.pub} and/or ~/.ssh/id_ed25519{,.pub}."""
 
+    # RSA-keys
     id_rsa = None
     id_rsa_pub = None
+
+    # ed25510-keys
+    id_ed25519 = None
+    id_ed25519_pub = None
+
     scan_hosts = Attribute(list, '')
-    provide_itself = Attribute(bool, True)
+    provide_itself = Attribute('literal', True)
+    purge_unmanaged_keys = Attribute('literal', False)
 
     def configure(self):
         if self.provide_itself:
@@ -24,17 +31,32 @@ class SSHKeyPair(Component):
 
         self += Directory('~/.ssh', mode=0o700)
 
+        # RSA
         if self.id_rsa:
             self += File('~/.ssh/id_rsa',
                          content=self.id_rsa,
                          mode=0o600)
-        else:
-            self += batou.c.common.AbsentFile('~/.ssh/id_rsa')
+        elif self.purge_unmanaged_keys:
+            self += Purge('~/.ssh/id_rsa')
 
         if self.id_rsa_pub:
             self += File('~/.ssh/id_rsa.pub',
                          content=self.id_rsa_pub)
 
+        # ED25519
+        if self.id_ed25519:
+            self += File('~/.ssh/id_ed25519',
+                         content='{}\n'.format(self.id_ed25519),
+                         mode=0o600)
+
+        elif self.purge_unmanaged_keys:
+            self += Purge('~/.ssh/id_ed25519')
+
+        if self.id_ed25519_pub:
+            self += File('~/.ssh/id_ed25519.pub',
+                         content=self.id_ed25519_pub)
+
+        # ScanHost
         for host in self.scan_hosts:
             self += ScanHost(host)
 
@@ -65,4 +87,3 @@ class ScanHost(Component):
             pass
         self.cmd('ssh-keyscan "{}" >> "{}"'.format(
                  self.hostname, self.known_hosts))
-
