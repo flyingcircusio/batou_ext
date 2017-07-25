4dbe8876a4e06f1261cf756f098aab77b57966cf
Frank Lanitz <fl@flyingcircus.io>
2017-07-25 12:32:16 +0200

    Change output format for via CustomizeYaml created yaml-files

--- src/batou_ext/config.py
+++ src/batou_ext/config.py
@@ -108,4 +108,4 @@ class CustomizeYaml(batou.component.Component):
 
     def update(self):
         with open(self.target, 'wb') as f:
-            yaml.dump(self._config, f)
+            yaml.dump(self._config, f, canonical=True)
