c8e775d9a2d57a2242f49b0253baf7bfb5ad090d
Christian Zagrodnick <cz@flyingcircus.io>
2017-12-20 08:14:36 +0100

    urlparse requires a protocol, which git "urls" lack.

--- src/batou_ext/git.py
+++ src/batou_ext/git.py
@@ -56,7 +56,8 @@ class GitCheckout(batou.component.Component):
 
         if self.scan_host:
             if not self.git_host:
-                self.git_host = urlparse.urlparse(self.git_clone_url).hostname
+                self.git_host = urlparse.urlparse(
+                    'ssh://' + self.git_clone_url).hostname
             # Add remote host to known hosts
             self += batou_ext.ssh.ScanHost(self.git_host, port=self.git_port)
 
