04d23b203b9eca639ce35ab3d8e8652a87ea1b29
Frank Lanitz <fl@flyingcircus.io>
2018-10-10 15:02:25 +0200

    Add a dummy component helping to manage access to NFS shares on client and server

---
+++ src/batou_ext/nfs.py
@@ -0,0 +1,20 @@
+import batou.component
+
+
+class NFS(batou.component.Component):
+    """
+    A component to help ensure you got access paths for NFS in sync
+    for your deployment.
+
+    Defaults are based on Flyingcircus' NixOS environment.
+    """
+
+    # Path where NFS on client is mounted on
+    basepath = batou.component.Attribute(str, '/mnt/nfs/shared')
+
+    # Path where NFS-share is located on the NFS server
+    serverpath = batou.component.Attribute(str, '/srv/nfs/shared')
+
+    def configure(self):
+
+        self.provide('nfs', self)
