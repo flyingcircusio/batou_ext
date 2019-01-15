86f7b23814d77500a802eaf4a3e4aa30b0810bf0
Frank Lanitz <fl@flyingcircus.io>
2019-01-15 15:54:33 +0100

    MySQL: Add generic component for default MySQL setups for typical LAMP projects

---
+++ src/batou_ext/mysql.py
@@ -0,0 +1,40 @@
+import batou.component
+import batou.lib.mysql
+
+
+class MySQLGeneric(batou.component.Component):
+    """
+    Allows to create default database as used e.g. for typical LAMP-projects
+
+    Usage:
+    self += batou_ext.mysql.MySQLGeneric(
+        'mydb',
+        username='myuser',
+        password='mypasswod',
+        admin_password='myadminpassword',
+        allow_from_host_name='127.0.0.1')
+    """
+    namevar = database
+    username = None
+    password = None
+    admin_password = None
+
+    # Used for GRANT-string
+    allow_from_hostname = batou.component.Attribute(str, 'localhost')
+
+    def configure(self):
+        self.provide('mysql', self)
+
+        self += batou.lib.mysql.Database(
+            self.database,
+            admin_password=self.admin_password)
+        self += batou.lib.mysql.User(
+            self.username,
+            password=self.password,
+            admin_password=self.admin_password,
+            allow_from_hostname=self.allow_from_hostname)
+        self += batou.lib.mysql.Grant(
+            self.database,
+            user=self.username,
+            admin_password=self.admin_password,
+            allow_from_hostname=self.allow_from_hostname)
