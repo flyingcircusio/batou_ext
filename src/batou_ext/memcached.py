aa7589c42b316ff835b97c35f4d153219f589a5f
Frank Lanitz <fl@flyingcircus.io>
2019-01-28 11:19:18 +0100

    Add a component for Memcached

---
+++ src/batou_ext/memcached.py
@@ -0,0 +1,41 @@
+import batou.component
+import batou.lib.file
+import batou.utils
+import batou_ext.config
+import batou_ext.nix
+import json
+
+
+@batou_ext.nix.rebuild
+class Memcached(batou.component.Component):
+    """
+    Helps to configure memcached
+
+    Usage:
+    self += batou_ext.memached.Memcached(
+        port=11211,
+        custom_config=dict(foo='baa', zzz=11))
+    """
+    port = batou.component.Attribute(int, 11211)
+
+    # A additional dict
+    custom_config = {}
+
+    def configure(self):
+        self.provide('memcached', self)
+        self.address = batou.utils.Address(self.host.fqdn, self.port)
+
+        # Trying to set some sane defaults
+        base_config = dict(
+            maxMemory='1024',
+            port=self.port,
+            maxConnections='1024'
+        )
+
+        config = batou_ext.config.dict_merge(
+            base_config,
+            self.custom_config)
+
+        self += batou.lib.file.File(
+            '/etc/local/memcached/memcached.json',
+            content=json.dumps(config))
