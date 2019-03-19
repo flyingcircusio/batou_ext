38a84ef43290dfb3c9848927e71982313a6e6bcb
Christian Zagrodnick <cz@flyingcircus.io>
2019-03-19 11:24:52 +0100

    add redis component

---
+++ src/batou_ext/redis.py
@@ -0,0 +1,22 @@
+import batou.component
+import batou.lib.file
+import batou.utils
+import batou_ext.nix
+
+
+@batou_ext.nix.rebuild
+class Redis(batou.component.Component):
+    """Component to define Redis password and address
+    """
+
+    password = None
+
+    password_file = "/etc/local/redis/password"
+    port = 6379
+
+    def configure(self):
+        assert self.password
+        self.provide("redis", self)
+
+        self.address = batou.utils.Address(self.host.fqdn, self.port)
+        self += batou.lib.file.File(self.password_file, content=self.password)
