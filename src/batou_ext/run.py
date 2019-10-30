7e0576ac8911e0a494f91d0930905a6b45434753
Frank Lanitz <fl@flyingcircus.io>
2019-10-30 15:48:43 +0100

    run.Run: Allow to executed provided scripts directly
    This might will be replaced by a new component from batou itself.
    -> https://github.com/flyingcircusio/batou/issues/14

--- src/batou_ext/run.py
+++ src/batou_ext/run.py
@@ -16,18 +16,29 @@ class Run(batou.component.Component):
         'hello.sh',
         content=script_content)
 
+    You can also provide ready-to-use-script by yourself:
+
+    self += batou.lib.file.File('myscript', mode=0o744)
+    self += batou_ext.run.Run('myscript', file=self._)
+
     The script is called only, if there are any changes at the parent component.
     """
     namevar = 'command'
     content = None
+    file = None
 
     def configure(self):
 
-        self += batou.lib.file.File(
-            self.command,
-            content=self.content,
-            mode=0o700)
-        self.command_file = self._
+        if not self.file and self.content:
+            self += batou.lib.file.File(
+                self.command,
+                content=self.content,
+                mode=0o700)
+            self.command_file = self._
+        elif self.file:
+            self.command_file = self.file
+        else:
+            raise ValueError("You need to provide either content or file.")
 
     def verify(self):
         self.parent.assert_no_changes()
