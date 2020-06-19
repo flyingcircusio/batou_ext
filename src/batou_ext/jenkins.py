0ab881616f2d587f48b8561508ee30ef68f27c3f
Christian Theune <ct@flyingcircus.io>
2020-06-19 21:37:32 +0200

    Fix str/bytes confusion after Python 3 migration

--- src/batou_ext/jenkins.py
+++ src/batou_ext/jenkins.py
@@ -31,6 +31,7 @@ def git_resolve(url, version):
         cmd = subprocess.Popen(['git', 'ls-remote', url, version],
                                 stdout=subprocess.PIPE)
         stdout, stderr = cmd.communicate()
+    stdout = stdout.decode('ascii')
     return stdout.split('\t', 1)[0]
 
 
