b3b3610b908ddeb69ec35e5259fd915880ed32cb
Frank Lanitz <fl@flyingcircus.io>
2022-06-28 11:00:51 +0200

    Add a component to make it easier to configure a journalbeat on our NixOS environment to ship logs to a centralized graylog server

---
+++ src/batou_ext/journalbeat.py
@@ -0,0 +1,21 @@
+import batou.component
+import batou.lib.file
+import pkg_resources
+
+
+class JournalBeatTransport(batou.component.Component):
+
+    """ A shortcut for sending logmessages via journalbeat to a centralied loghost.
+    """
+
+    nix_file_path = batou.component.Attribute(str, default="/etc/local/nixos/journalbeat.nix")
+    transport_name = batou.component.Attribute(str)
+    graylog_host = batou.component.Attribute(str)
+    graylog_port = batou.component.Attribute(int, default=12301)
+
+    def configure(self):
+
+        self += batou.lib.file.File(
+            self.nix_file_path,
+            content=pkg_resources.resource_string(
+                __name__, "resources/journalbeat.nix"))
