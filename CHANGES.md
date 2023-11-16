500ea808966edc8a3f86f340efa42479cf9512b1
Christian Zagrodnick <cz@flyingcircus.io>
2023-11-16 15:20:35 +0100

    Preparing release 2.4.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,4 @@
 
-### 0.1 (unreleased)
+## 2.4.0 (2023-11-16)
 
 - Add release process with changelog (FC-33250).
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def project_path(*names):
 
 setup(
     name="batou_ext",
-    version="2.3.dev0",
+    version="2.4.0",
     install_requires=[
         "batou >= 2.3b4",
         "pyaml",
