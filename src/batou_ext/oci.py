92535dd0fbe1753087800819dccac5c46dbcfcfc
Christian Zagrodnick <cz@flyingcircus.io>
2024-01-29 07:55:24 +0100

    Fix bug where `batou` was not imported and improve OCI codestyle

--- src/batou_ext/oci.py
+++ src/batou_ext/oci.py
@@ -1,62 +1,61 @@
-import hashlib
 import os
-import re
 import shlex
+from textwrap import dedent
 from typing import Optional
 
 import batou
-import pkg_resources
 from batou import UpdateNeeded
 from batou.component import Attribute, Component
 from batou.lib.file import File
-from batou.lib.service import Service
 
 import batou_ext.nix
 
 
 @batou_ext.nix.rebuild
 class Container(Component):
-    """
-    A OCI Container component.
-    With this component you can dynamically schedule docker containers to be run on the target host
+    """A OCI Container component.
+
+    With this component you can dynamically schedule docker containers to be
+    run on the target host.
 
     Note: Docker image specifiers do not follow a properly resolvable pattern.
-        Therefore, container registries have to be specified seperately if you need to log in before.
-        If you do not provide a container registry, docker will use the default one for authentication.
-        You can choose to also append the image attribute with the registry but this module will do so
-        automatically.
+    Therefore, container registries have to be specified seperately if you need
+    to log in before. If you do not provide a container registry, docker will
+    use the default one for authentication. You can choose to also append the
+    image attribute with the registry but this module will do so automatically.
 
-        ```
-        # the following two call are identical:
-        self += batou_ext.oci.Container(
-            image="foo",
-            registry_address="test.registry",
-            registry_user="foo",
-            registry_password="bar")
+    ```
+    # the following two call are identical:
+    self += batou_ext.oci.Container(
+        image="foo",
+        registry_address="test.registry",
+        registry_user="foo",
+        registry_password="bar")
 
-        self += batou_ext.oci.Container(
-            image="test.registry/foo",
-            registry_address="test.registry",
-            registry_user="foo",
-            registry_password="bar")
+    self += batou_ext.oci.Container(
+        image="test.registry/foo",
+        registry_address="test.registry",
+        registry_user="foo",
+        registry_password="bar")
 
-        # However, this will fail since docker will try to log into the default container registry
-        # with the provided credentials and then pull the image from the registry specified in the image string
-        self += batou_ext.oci.Container(
-            image="test.registry/foo",
-            registry_user="foo",
-            registry_password="bar")
+    # However, this will fail since docker will try to log into the default container registry
+    # with the provided credentials and then pull the image from the registry specified in the image string
+    self += batou_ext.oci.Container(
+        image="test.registry/foo",
+        registry_user="foo",
+        registry_password="bar")
 
 
-        # if you don't need to authenticate, not explicitly specifying the registry is fine of course
-        # and it will pull the image from the correct registry
-        self += batou_ext.oci.Container(image="test.registry/foo")
-        ```
+    # if you don't need to authenticate, not explicitly specifying the registry is fine of course
+    # and it will pull the image from the correct registry
+    self += batou_ext.oci.Container(image="test.registry/foo")
+    ```
 
     Example:
     ```
     self += batou_ext.oci.Container(image = "mysql", version = "8.0")
     ```
+
     """
 
     # general options
@@ -86,7 +85,9 @@ class Container(Component):
             self.registry_user or self.registry_password
         ) and not self.registry_address:
             batou.output.annotate(
-                "WARN: you might want to specify the registry explicitly unless you really intend to log into the default docker registry"
+                "WARN: you might want to specify the registry explicitly"
+                " unless you really intend to log into the default"
+                " docker registry"
             )
 
         if self.version:
@@ -131,25 +132,39 @@ class Container(Component):
         if self.registry_address:
             logintxt, _ = self.cmd(
                 self.expand(
-                    """
-                    docker login \\
-                        {%- if component.registry_user and component.registry_password %}
-                        -u {{component.registry_user}} \\
-                        -p {{component.registry_password}} \\
-                        {%- endif %}
-                        {{component.registry_address}}
-                """
+                    dedent(
+                        """\
+        docker login \\
+            {%- if component.registry_user and component.registry_password %}
+            -u {{component.registry_user}} \\
+            -p {{component.registry_password}} \\
+            {%- endif %}
+            {{component.registry_address}}
+        """
+                    )
                 )
             )
 
         local_digest, stderr = self.cmd(
-            "docker image inspect {{component.image}} | jq -r 'first | .RepoDigests | first | split(\"@\") | last' || echo image not available locally"
+            dedent(
+                """\
+            docker image inspect {{component.image}} \
+                | jq -r 'first | .RepoDigests | first | split("@") | last' \
+                || echo image not available locally
+                """
+            )
         )
         remote_digest, stderr = self.cmd(
-            "docker manifest inspect {{component.image}} -v | jq -r 'if type ==\"array\" then (. | first) else . end | .Descriptor.digest'"
+            dedent(
+                """\
+            docker manifest inspect {{component.image}} -v \
+                | jq -r 'if type =="array" then (. | first) else . end | .Descriptor.digest'
+                """
+            )
         )
 
-        # `docker manifest inspect` silently raises an error, returns code 0 when unathorized
+        # `docker manifest inspect` silently raises an error, returns code 0
+        # when unathorized
         if stderr == "unauthorized":
             raise RuntimeError(
                 "Wrong credentials for remote container registry"
