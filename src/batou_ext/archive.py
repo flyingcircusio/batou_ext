f74cf1eb9059f7c5a1d8f5f27e63c76511cb1d29
Maximilian Bosch <mb@flyingcircus.io>
2024-07-08 15:33:15 +0200

    archive: optionally remove old delete markers from SingleUntar
    FC-26850

--- src/batou_ext/archive.py
+++ src/batou_ext/archive.py
@@ -1,6 +1,10 @@
+import os
 import os.path
+from glob import glob
+from time import time
 
 import batou.lib.archive
+from batou.component import Attribute
 
 
 class SingleUntar(batou.lib.archive.Untar):
@@ -15,6 +19,11 @@ class SingleUntar(batou.lib.archive.Untar):
 
     _required_params_ = {"target": "."}
 
+    cleanup_markers_after_days = Attribute(int, default=None)
+
+    _markers_to_unlink = []
+    _only_marker_cleanup = False
+
     def configure(self):
         super().configure()
         self._finish_marker = os.path.join(
@@ -23,12 +32,34 @@ class SingleUntar(batou.lib.archive.Untar):
         )
 
     def verify(self):
+        # If a deployment does this for a while, a lot of these markers accumulate.
+        # If this attribute is set, cleanup all markers with an mtime older than
+        # `cleanup_markers_after_days` days.
+        if self.cleanup_markers_after_days:
+            now = time()
+            diff = now - self.cleanup_markers_after_days * 24 * 3600
+
+            self._markers_to_unlink = [
+                file
+                for file in glob(self.map(".*.finished-extract"))
+                if os.path.getmtime(file) < diff
+            ]
+
         assert os.path.exists(self.archive)
         # Verify that the target dir is newer than the archive on disk.
         self.assert_file_is_current(self.target, [self.archive])
         # Verify that there is a finish marker and it is newer than the arvive
         self.assert_file_is_current(self._finish_marker, [self.archive])
 
+        # If we got that far, we don't have to unpack again. But we should
+        # clean up some old markers.
+        self._only_marker_cleanup = True
+        assert self._markers_to_unlink == []
+
     def update(self):
-        super().update()
-        self.touch(self._finish_marker)
+        if not self._only_marker_cleanup:
+            super().update()
+            self.touch(self._finish_marker)
+
+        for old_marker in self._markers_to_unlink:
+            os.remove(old_marker)
