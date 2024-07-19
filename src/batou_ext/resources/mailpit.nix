66cac1e3c116d2fab9caad985758a3347c0251f3
Maximilian Bosch <mb@flyingcircus.io>
2024-07-19 14:27:09 +0200

    mailpit: init component
    FC-39624
    
    For now it's just for a single customer, but considering that this is
    actively developed in contrast to mailhog, we may want to switch all
    setups to it in the future.

---
+++ src/batou_ext/resources/mailpit.nix
@@ -0,0 +1,24 @@
+{ lib, pkgs, ... }: {
+  systemd.services.mailpit = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      DynamicUser = true;
+      StateDirectory = "mailpit";
+      WorkingDirectory = "%S/mailpit";
+      ExecStart = "${lib.getExe pkgs.mailpit} -d mailpit.db -s \"[::]:{{component.smtp_port}}\" -l \"[::1]:{{component.ui_port}}\" --max {{component.max}}";
+      Restart = "on-failure";
+    };
+  };
+
+  flyingcircus.services.nginx.virtualHosts."{{ component.public_name }}" = {
+    forceSSL = true;
+    enableACME = true;
+    # {% if component.http_auth_enable %}
+    basicAuthFile = "{{component.http_auth.path}}";
+    # {% endif %}
+    locations."/" = {
+      proxyPass = "http://[::1]:8025";
+      proxyWebsockets = true;
+    };
+  };
+}
